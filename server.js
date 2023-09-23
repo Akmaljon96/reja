50938ca9c9a6a755992856669d7b3d23e6f4f3ee
Akmaljon96 <supermen9606@gmail.com>
2023-09-23 21:28:37 +0900

    feat:07dars

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const app = express();
 const http = require("http");
 
 //1-Kirish code
-
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //2- session
-
 //3-Views code
-
 app.set("views", "views");
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 //4-Routing code
-app.get("/hello", function(req, res){
-    res.end("<h1>hello world by Wayne!!!</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({ test: "success" });
 });
-app.get("/gift", function(req, res){
-    res.end("<h1>Siz sovg'alar sahifasidasiz </h1>");
-});
+
+app.get("/", function(req, res) {
+    console.log(req);
+  res.render("harid");
+});   
 
 const server = http.createServer(app); 
 let PORT =3000;
