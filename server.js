f584e7cd1373cec78736a73f103bd028155b3038
Akmaljon96 <supermen9606@gmail.com>
2023-09-29 15:32:11 +0900

    fix: refactor

--- server.js
+++ server.js
@@ -35,12 +35,12 @@ app.get('/author', (req, res) => {
 });
 
 app.get("/", function(req, res) {
-    console.log(req);
-  res.render("harid");
+    // console.log(req);
+  res.render("reja");
 });   
 
 const server = http.createServer(app); 
 let PORT =3000;
 server.listen(PORT, function() {
-    console.log(`The server is running succesfully on port: ${PORT}`);
+    console.log(`The server is running succesfully on port: ${PORT}, http://localhost:{PORT}`);
 });
