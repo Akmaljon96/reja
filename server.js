62cc53fdfc197d90c46bdd7bd299a77cb1836bb8
Akmaljon96 <supermen9606@gmail.com>
2023-09-30 21:34:56 +0900

    feat: mongodb ulanish logic

--- server.js
+++ server.js
@@ -1,46 +1,26 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else{
-        user = JSON.parse(data)
+let db;
+const connectionString = "mongodb+srv://supermen96:madina98$@cluster0.calopfh.mongodb.net/"
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connectioon MongoDB");
+    else {
+        console.log("MongoDB connection succeed");
+        console.log(client);
+        const app = require("./app");
+        const server = http.createServer(app); 
+        let PORT =3000;
+        server.listen(PORT, function() {
+        console.log(
+            `The server is running succesfully on port: ${PORT}, http://localhost:{PORT}`
+            );
+         });
     }
-});
+ }
+);
 
-//1-Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-//2- session
-//3-Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4-Routing code 
-app.post("/create-item", (req, res) => {
-    //console.log(req);
-    //res.json({ test: "success" });
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user} );
-});
-
-app.get("/", function(req, res) {
-    // console.log(req);
-  res.render("reja");
-});   
-
-const server = http.createServer(app); 
-let PORT =3000;
-server.listen(PORT, function() {
-    console.log(`The server is running succesfully on port: ${PORT}, http://localhost:{PORT}`);
-});
--- train.js
+++ train.js
@@ -1,3 +1,21 @@
+ //  📌  E-TASK: E-Task: Shunday function tuzing, u bitta string argumentni qabul qilib osh stringni teskari qilib return qilsin
+
+//masalan: getReverse("hello") return qilsin "olleh"
+
+function getReverse(inputString) {
+  return inputString.split('').reverse().join('');
+}
+
+var reversedString = getReverse("kiyikk");
+console.log(reversedString); 
+
+
+
+
+
+
+
+
 // // 9-Dars
 
 
@@ -37,15 +55,15 @@
 // 10-Dars
 
 
-console.log("Jack Ma maslahatlari");
-const list =[
-  "yahshi talaba boling", //0-20
-  "togri boshliq tanlang va koproq xato qiling", //20-30
-  "uzingizga ishlashni boshlang", //30-40
-  "siz kuchli bo'lgan narsalarni qiling", //40-50
-  "yoshlarga investitsiya qiling", //50-60
-  "endi dam oling, foydasi yoq", //60-
-];
+//console.log("Jack Ma maslahatlari");
+//const list =[
+//   "yahshi talaba boling", //0-20
+//   "togri boshliq tanlang va koproq xato qiling", //20-30
+//   "uzingizga ishlashni boshlang", //30-40
+//   "siz kuchli bo'lgan narsalarni qiling", //40-50
+//   "yoshlarga investitsiya qiling", //50-60
+//   "endi dam oling, foydasi yoq", //60-
+// ];
 //ASYNC function
 // async function maslahatBering(a) {
 //    if(typeof a !== 'number') throw new Error("insert a number");
@@ -85,25 +103,27 @@ const list =[
 // run();
 
 //CALLBACK function
- function maslahatBering(a, callback) {
-     if(typeof a !== 'number') callback("Insert a number",null);
-     else if(a <= 20) callback(null, list[0]);
-     else if(a > 20 && a <= 30) callback(null, list[1]);
-     else if(a > 30 && a <= 40) callback(null, list[2]);
-     else if(a > 40 && a <= 50) callback(null, list[3]);
-     else if(a > 50 && a <= 60) callback(null, list[4]);
-     else{
-      setInterval(function () {
-      callback(null, list[5]);
-     }, 3000);
-    }
-  }
-  console.log("passed here 0");
-  maslahatBering(65, (err, data) => {
-    if (err) console.log("ERROR:", err);
-    else{
-      console.log("javob", data);
-    }
-  });
-  console.log("passed here 1");
+//  function maslahatBering(a, callback) {
+//      if(typeof a !== 'number') callback("Insert a number",null);
+//      else if(a <= 20) callback(null, list[0]);
+//      else if(a > 20 && a <= 30) callback(null, list[1]);
+//      else if(a > 30 && a <= 40) callback(null, list[2]);
+//      else if(a > 40 && a <= 50) callback(null, list[3]);
+//      else if(a > 50 && a <= 60) callback(null, list[4]);
+//      else{
+//       setInterval(function () {
+//       callback(null, list[5]);
+//      }, 3000);
+//     }
+//   }
+//   console.log("passed here 0");
+//   maslahatBering(65, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else{
+//       console.log("javob", data);
+//     }
+//   });
+//   console.log("passed here 1");
   
+
+ 
