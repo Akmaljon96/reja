29df95b89ad2dbbe810a153c901e05b12fc4a229
Akmaljon96 <supermen9606@gmail.com>
2023-10-03 20:51:19 +0900

    feat: tuzatildi

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-console.log("Web Serverni boshlash");
+// console.log('Web serverni boshlash');
 const express = require("express");
-// const res = require("express/lib/response");
 const app = express();
 
-
-//MongoDB connect
+const fs = require("fs");
+// MongoDB chaqirish
 const db = require("./server").db();
 
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data)=>{
+    if(err){
+        console.log("ERROR:", err);
+    }else{
+        user = JSON.parse(data);
+    }
+});
 
-//1-Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// 1: Kirish kodlari. expressga kirib kelayotgan ma'lumotlarga bog'liq bo'lgan kodlar yoziladi.
+app.use(express.static("public")); // chrome express ga request qilayotgan payti public folderni clientlarga ochib beradi.
+app.use(express.json()); //kirib kelayotgan json farmatidagi datani objectga ugirib beradi.
+app.use(express.urlencoded({extended: false})); //html dan form request (form data post qilsa express qabul qilib oladi)
 
-//2- session
-//3-Views code
+// 2 Session larga bog'liq bo'lgan codelar yoziladi
+
+// 3 View code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4-Routing code 
-app.post("/create-item", (req, res) => {
-  console.log('user entered /create-item');
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-    console.log(data.ops);
-    res.json(data.ops[0]);
-  }); 
-});
-
-  //  console.log(req.body);
-  
-app.get('/author', (req, res) => {
-//     res.render("author", {user: user} );
- });
-
-app.get("/", function(req, res) {
-  console.log('user entered /');
-  db.collection("plans")
-   .find()
-    .toArray((err, data) => {
-      if(err) {
-       console.log(err);
-       res.end("something went wrong");
-      } else {
-        // console.log(data);
-        res.render("reja", { items: data });
-      }
+// 4 Routing code
+app.post('/create-item', (req, res)=>{
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        console.log(data.ops);
+        res.json(data.ops[0]);
     });
-});   
+});
+app.get("/author", (req, res)=>{
+    res.render("author", {user: user});
+})
+app.get("/", function(req, res){
+    console.log("user entered /");
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err){
+            console.log(err);
+            res.end("something went wrong");
+        }else{
+            res.render("reja", {items: data});
+        }
+    });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-const http = require("http");
+const http = require('http');
+
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = 
-"mongodb+srv://supermen96:madina98$@cluster0.calopfh.mongodb.net/"
+const connectionString = "mongodb+srv://supermen96:madina98$@cluster0.calopfh.mongodb.net/";
 
-mongodb.connect(
-    connectionString, 
-    {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-}, 
- (err, client) => {
-    if(err) console.log("ERROR on connectioon MongoDB");
-    else {
-        console.log("MongoDB connection succeed");
-       module.exports = client;
-
-
-        const app = require("./app");
-        const server = http.createServer(app); 
-        let PORT =3000;
-        server.listen(PORT, function() {
-        console.log(
-            `The server is running succesfully on port: ${PORT}, http://localhost:{PORT}`
-            );
-         });
-    }
- }
-);
-
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+    }, (err, client) => {
+        if(err) console.log("ERROR on connection MongoDB");
+        else{
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+            const app = require('./app');
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function(){
+                console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+});
+        }
+    })
